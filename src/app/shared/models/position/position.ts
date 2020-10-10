09b48931053ff3bef9bfcf901923874f0c87c847
EOussama <oussama_gtacj@live.fr>
2020-10-10 11:11:57 +0100

    Added position

--- src/app/shared/models/position/position.ts
+++ src/app/shared/models/position/position.ts
@@ -1,2 +1,23 @@
-export class Position {
+export interface IPosition {
+  x: number;
+  y: number;
+}
+
+export class Position implements IPosition {
+
+  //#region Properties
+
+  x: number;
+  y: number;
+
+  //#endregion
+
+  //#region Lifecycle
+
+  constructor(position: IPosition) {
+    this.x = position?.x ?? 0;
+    this.y = position?.y ?? 0;
+  }
+
+  //#endregion
 }
