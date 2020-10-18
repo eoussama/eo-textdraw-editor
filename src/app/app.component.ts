1cff2938d969520af0a7548b4f60d2fce7a7ff37
EOussama <oussama_gtacj@live.fr>
2020-10-18 12:08:00 +0100

    Added board element creator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ElementType } from './shared/enums/element-types.enum';
 import { Board } from './shared/models/board/board';
 import { Dimension } from './shared/models/dimension/dimension';
 import { BoxElement } from './shared/models/elements/box-element';
@@ -21,15 +22,15 @@ export class AppComponent implements OnInit {
   //#region Lifecycle
 
   ngOnInit(): void {
-    this.board = new Board({
-      dimension: new Dimension({ height: 420, width: 720 }),
-      elements: [
-        new BoxElement({ backgroundColor: '#FFF0F0', position: new Position({ y: 236 }) }),
-        new BoxElement({ position: new Position({ x: 186, y: 58 }), dimension: new Dimension({ width: 200, height: 39 }) }),
-        new TextElement({ text: 'Textdraw Text', color: '#FFB226' }),
-        new TextElement({ text: 'Example Text 2', position: new Position({ x: 220, y: 356 }) }),
-      ]
-    });
+    this.board = new Board({ dimension: new Dimension({ height: 420, width: 720 }) });
+
+    [
+      { type: ElementType.Box, backgroundColor: '#FFF0F0', position: new Position({ y: 236 }) },
+      { type: ElementType.Box, position: new Position({ x: 186, y: 58 }), dimension: new Dimension({ width: 200, height: 39 }) },
+      { type: ElementType.Text, text: 'Textdraw Text', color: '#FFB226' },
+      { type: ElementType.Text, text: 'Example Text 2', position: new Position({ x: 220, y: 356 }) },
+    ].forEach((element) => this.board.createElement(element.type, element));
+    // this.board.createElement(ElementType.Text);
   }
 
   //#endregion
--- src/app/shared/models/board/board.ts
+++ src/app/shared/models/board/board.ts
@@ -1,7 +1,9 @@
 import { v4 as uuidv4 } from 'uuid';
+import { ElementType } from '../../enums/element-types.enum';
+import { getElementClass } from '../../utils/element.util';
 
 import { Dimension } from '../dimension/dimension';
-import { Element } from '../elements/element';
+import { Element, IElement } from '../elements/element';
 
 export interface IBoard {
   readonly id: string;
@@ -28,6 +30,18 @@ export class Board implements IBoard {
 
     this.dimension = { ...(board?.dimension ?? new Dimension({ height: 720, width: 1280 })) };
     this.elements = [...(board?.elements ?? [])];
+  }
+
+  //#endregion
+
+  //#region Methods
+
+  createElement(type: ElementType = ElementType.Box, element?: Partial<IElement>) {
+    const GenericElement = getElementClass(type);
+
+    this.elements.push(
+      new GenericElement({ ...element })
+    );
   }
 
   //#endregion
