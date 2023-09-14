6388d8e0384325d2349fafc92971954e8e640408
EOussama <oussama_gtacj@live.fr>
2023-09-14 23:13:55 +0100

    📦 Box Component, #52

---
+++ core/component/box.ts
@@ -0,0 +1,29 @@
+import { Component } from '.';
+import { TBoxProps } from '../utils/types/props/boxProps.type';
+
+
+/**
+ * @description
+ * The box of the entity on the screen
+ */
+export class BoxComponent extends Component {
+
+  /**
+   * @description
+   * The box color of the entity
+   */
+  boxColor: string;
+
+  /**
+   * @description
+   * The box state of the entity
+   */
+  useBox: boolean;
+
+  constructor(props?: Partial<TBoxProps>) {
+    super();
+
+    this.useBox = props?.useBox ?? true;
+    this.boxColor = props?.boxColor ?? '#000000FF';
+  }
+}
---
+++ core/utils/types/props/boxProps.type.ts
@@ -0,0 +1,18 @@
+/**
+ * @description
+ * Box component properties
+ */
+export type TBoxProps = {
+
+  /**
+   * @description
+   * The box color of the entity
+   */
+  boxColor: string;
+
+  /**
+   * @description
+   * The box state of the entity
+   */
+  useBox: boolean
+}
