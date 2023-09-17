c6dfc6da88b9c2e61abd4d40c0de17a2b6754d1d
EOussama <oussama_gtacj@live.fr>
2023-09-17 15:00:12 +0100

    🖱️ Directional Resize

--- hooks/resizeable.hook.tsx
+++ hooks/resizeable.hook.tsx
@@ -1,5 +1,7 @@
 import styles from './../components/textdraw/TextDraw.module.scss';
-import { MutableRefObject, useMemo } from 'react';
+
+import { MutableRefObject, useEffect, useMemo, useState } from 'react';
+
 import { useResize } from './resize.hook';
 import { TextDraw } from '../core/entity/textdraw';
 import { TextComponent } from '../core/component/text';
@@ -15,7 +17,8 @@ import { TextComponent } from '../core/component/text';
 export function useResizeable(textdraw: TextDraw, parentRef: MutableRefObject<HTMLDivElement>) {
   const textdrawtext = useMemo(() => textdraw.getComponent(TextComponent), [textdraw]);
   const { width, height, minWidth, maxWidth, minHeight, maxHeight, isResizing, setIsResizing, onResize } = useResize(textdraw, parentRef);
-  
+  const [resizeAngle, setResizeAngle] = useState('se');
+
   const resizeableSize = {
     width: textdrawtext ? 0 : width,
     height: textdrawtext ? 0 : height
@@ -26,14 +29,22 @@ export function useResizeable(textdraw: TextDraw, parentRef: MutableRefObject<HT
     maxConstraints: [maxWidth, maxHeight]
   };
 
+  useEffect(() => {
+    console.log({ resizeAngle });
+  }, [resizeAngle]);
+
   const resizeableHandle: any = {
-    handleSize: [8, 8],
-    handle: !textdrawtext &&
-      <span
-        className={styles['handle']}
-        onMouseEnter={() => setIsResizing(true)}
-        onMouseLeave={() => setIsResizing(false)}
-      />
+    handleSize: [10, 10],
+    resizeHandles: [resizeAngle],
+    handle: !textdrawtext && <div>
+      {['n', 'e', 's', 'w', 'ne', 'se', 'nw', 'sw'].map(direction =>
+        <span
+          className={`${styles['handle']} ${styles[`handle--${direction}`]}`}
+          onMouseEnter={() => { setIsResizing(true); setResizeAngle(direction); }}
+          onMouseLeave={() => setIsResizing(false)}
+        />
+      )}
+    </div>
   };
 
   const resizeableCallbacks = {
